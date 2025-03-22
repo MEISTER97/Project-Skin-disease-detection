a49d0bccf70a9d2bba9e8f715715c1265d521bd5
Man Tran <139363311+MEISTER97@users.noreply.github.com>
2025-03-22 10:02:47 +0200

    Delete Django_api/Django_api directory

--- Django_api/Django_api/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import upload_image, upload_success_view
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_image, name='upload_image'),
-    path("upload/success/<int:prediction_id>/", upload_success_view, name="upload_success_view"),
-]
