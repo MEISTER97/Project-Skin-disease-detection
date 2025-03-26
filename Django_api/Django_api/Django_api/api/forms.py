ac533729bd725d3c8f6a5592e4dc4c8807a8cdcf
Man Tran <139363311+MEISTER97@users.noreply.github.com>
2025-03-26 08:52:16 +0200

    Delete Django_api directory

--- Django_api/Django_api/Django_api/api/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ImagePrediction
-
-class ImageUploadForm(forms.ModelForm):
-    class Meta:
-        model = ImagePrediction
-        fields = ['image']
